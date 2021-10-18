80a0478338e92974d88424609a3046eea090c254
lukmanharun1 <54461081+lukmanharun1@users.noreply.github.com>
2021-10-18 16:30:14 +0700

    security using helmet

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express')
 require('dotenv').config();
 const app = express();
 const routes = require('./routes/index');
+const helmet = require('helmet')
+app.use(helmet())
+
 app.use(express.json());
 app.use(routes);
 
 const port = process.env.PORT;
 app.listen(port, () => {
-    console.log(`Person listening at http://localhost:${port}`)
-  })
+  console.log(`Person listening at http://localhost:${port}`)
+})
 module.exports = app;
 
